7817cc04a5bd065c43121277f5db55bd3bfdded3
OussamaBenakmoum <io_benakmoum@esi.dz>
2021-08-15 21:56:23 +0100

    Ajoutez des fichiers projet.

---
+++ GestionMaintenance/Models/TypeAppareil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLite.Net.Attributes;
+
+
+namespace GestionMaintenance.Models
+{
+    public class TypeAppareil
+    {
+        [PrimaryKey, AutoIncrement]
+         
+        private int id { get; set; }
+        private string nom { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{id} - {nom}";
+        }
+    }
+}
